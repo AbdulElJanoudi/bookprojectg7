6155f4b79b3c791358ee1655cd155803dce4ea84
Abdul El Janoudi <abbe_201@hotmail.com>
2020-05-19 15:14:54 +0200

    Deploying React app to github

---
+++ README.old.md
@@ -0,0 +1,2 @@
+# BookProjectG7
+A project in React JS utilizing the "Google Books APIs". This project is made for educational purpose in the course "Flerplattformsapplikationer med webbtekniker"
---
+++ src/App.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <h1> Our React App</h1>
+      <h3> Authors: Abdul, Simon, Sofie</h3>
+    </div>
+  );
+}
+
+export default App;
