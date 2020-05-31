f2392306bdac91c1a8dae6d6ee8efd89ebf5e08d
Abdul El Janoudi <abbe_201@hotmail.com>
2020-05-31 21:45:24 +0200

    buggfixar

--- src/App.js
+++ src/App.js
@@ -4,7 +4,9 @@ import Books from "./components/Books"
 
 const apiKey = "AIzaSyDc84mT8wa9h5Tcf8Kp17u1z-6eq201eFQ";
 
+
 class App extends Component {
+
   state = {
     books: [],
     error: null
@@ -16,27 +18,31 @@ class App extends Component {
     const data = await apiCall.json();
 
     if (!bookName) {
-      this.setState({ error: "Vänligen fyll i sökfältet"})
+      this.setState({ error: "Vänligen fyll i sökfältet" })
     } else if (!data.items) {
-      this.setState ({ error: "Inga böcker hittades"})
+      this.setState({ error: "Inga böcker hittades" })
     } else {
-      this.setState({books: data.items, error: null})
+      this.setState({ books: data.items, error: null })
     }
-
-    console.log(this.state.books);
-  }
   
+  for (const theEntireBook of this.state.books) {
+    let allMyIds = theEntireBook.id;
+    return allMyIds
+  }
+}
+
   render() {
     return (
-          <main>
-            <Form getBook={this.getBook} />
-            {
-              this.state.error !== null ? 
-              <div id="errorMsg">
-              { this.state.error}
-              </div> : <Books books={this.state.books} />
-            }
-          </main>
+      <main>
+        <Form getBook={this.getBook} />
+        {
+          this.state.error !== null ?
+            <div id="errorMsg">
+              {this.state.error}
+            </div> : <Books books={this.state.books} />
+        }
+
+      </main>
     );
   }
 }
