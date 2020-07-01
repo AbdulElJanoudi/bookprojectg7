f38058d057db98f3bc2c55bdf5ff5cbc55274d39
Sofie <sofieljungcrantz@hotmail.com>
2020-07-01 21:41:53 +0200

    Fixat problem vid uppdatering av biblioteket

--- src/components/BookLib.js
+++ src/components/BookLib.js
@@ -3,10 +3,17 @@ import React from 'react'
 var myData = {}
 
 async function getMyBooks() {
+    let myBooks = document.getElementById("myBooks");
+    if (myBooks) {
+        myBooks.innerHTML = '';
+    } else {
+        myBooks = document.createElement('div');
+        myBooks.setAttribute("id", "myBooks")
+        document.body.appendChild(myBooks);
+    }
     for (var i = 0; i < localStorage.length; i++) {
         var favBooks = (localStorage.getItem(localStorage.key(i)));
         var favBooksParsed = JSON.parse(favBooks)
-
         for (i = 0; i < favBooksParsed.length; i++) {
             const url = (`https://www.googleapis.com/books/v1/volumes/${favBooksParsed[i]}?key=AIzaSyDc84mT8wa9h5Tcf8Kp17u1z-6eq201eFQ`);
             const response = await fetch(url);
@@ -21,7 +28,7 @@ async function getMyBooks() {
             myFigure.appendChild(myP)
             myP.innerHTML = (myData.content.volumeInfo.title)
             img.src = (myData.content.volumeInfo.imageLinks.thumbnail)
-            document.body.appendChild(myDiv)
+            myBooks.appendChild(myDiv)
             myDiv.appendChild(myFigure)
         }
     }
@@ -33,4 +40,3 @@ export default function BookLib() {
         <> </>
     );  
 }
-
